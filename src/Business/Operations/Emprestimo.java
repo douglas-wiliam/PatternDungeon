21d4a2a8df90d0aac8806bb101bffd3d4463e359
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-08 20:35:38 -0300

    Classe movida para pacote Business.Operations

---
+++ src/Business/Operations/Emprestimo.java
@@ -0,0 +1,25 @@
+package Business.Operations;
+
+/**
+ *
+ * @author douglas
+ */
+public class Emprestimo {
+
+    String codigoUsuario;
+    String codigoLivro;
+
+    public Emprestimo(String codigoUsuario, String codigoLivro) {
+        this.codigoUsuario = codigoUsuario;
+        this.codigoLivro = codigoLivro;
+    }
+
+    public String getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public String getCodigoLivro() {
+        return codigoLivro;
+    }
+
+}
