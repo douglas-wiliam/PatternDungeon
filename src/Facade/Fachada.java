b0d0861c9288bcdd51587950cf5e8e16604dbed0
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-08 23:54:07 -0300

    Implementacao do metodo realizaEmprestimo

--- src/Facade/Fachada.java
+++ src/Facade/Fachada.java
@@ -1,5 +1,11 @@
 package Facade;
 
+import Business.Biblioteca;
+import Business.Operations.Emprestimo;
+import Business.Operations.Pedido;
+import Business.Users.Usuario;
+import Validate.Checker;
+
 /**
  *
  * @author douglas
@@ -18,8 +24,25 @@ public class Fachada {
         return uniqueInstance;
     }
 
-    public static String realizaEmprestimo(String codigoUsuario, String codigoLivro) {
-        // Implementar
-        return "Realizar Empréstimo não Implementado";
+    public static String realizaEmprestimo(Pedido pedidoEmprestimo) {
+        // Supondo que os usuarios sejam validos
+        Usuario usuario = Biblioteca.buscaUsuario(pedidoEmprestimo.getCodigoUsuario());
+        Emprestimo emprestimo;
+        
+        usuario.setPedido(pedidoEmprestimo);
+
+        for (Checker c : usuario.getCheckers()) {
+            c.check();
+            if (!"".equals(c.getLog())) {
+                usuario.setPedido(null);
+                return c.getLog();
+            }
+        }
+
+        usuario.setPedido(null);
+        emprestimo = new Emprestimo(pedidoEmprestimo.getCodigoUsuario(), pedidoEmprestimo.getCodigoLivro());
+        usuario.addEmprestimo(emprestimo);
+        Biblioteca.addEmprestimo(emprestimo);
+        return "Empréstimo realizado com sucesso";
     }
 }
