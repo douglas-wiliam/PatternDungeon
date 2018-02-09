026fc60fc83a4ebc19fde1dcc7d71ead343c59a9
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-09 08:34:34 -0300

    ArrayList emprestimos e o metodo addEmprestimos retirados

--- src/Business/Biblioteca.java
+++ src/Business/Biblioteca.java
@@ -1,6 +1,5 @@
 package Business;
 
-import Business.Operations.Emprestimo;
 import Business.Users.Usuario;
 import java.util.ArrayList;
 
@@ -12,7 +11,6 @@ public class Biblioteca {
 
     private static Biblioteca uniqueInstance;
     private static ArrayList<Usuario> usuarios;
-    private static ArrayList<Emprestimo> emprestimos;
 
     private Biblioteca() {
 
@@ -32,10 +30,6 @@ public class Biblioteca {
 
     public static Livro buscaLivro(String codigoLivro) {
         throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public static void addEmprestimo(Emprestimo emprestimo) {
-        emprestimos.add(emprestimo);
     }
 
     public static void addUsuario(Usuario usuario) {
