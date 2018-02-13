2a831de7e8fef8a1f3bbb4aba8080fd74f4af073
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-13 20:37:18 -0300

    Removida variavel mapCommands e todos os metodos relacionados

--- src/Facade/ComandoInvocador.java
+++ src/Facade/ComandoInvocador.java
@@ -10,26 +10,11 @@ import Commands.Comando;
  */
 public class ComandoInvocador {
 
-    private Map mapCommands;
-
     public ComandoInvocador() {
-        mapCommands = new HashMap();
     }
 
     public String executeCommand(Comando comando) {
         return comando.execute();
-    }
-
-    public void addCommand(Comando comando, String codigo) {
-        mapCommands.put(codigo, comando);
-    }
-
-    public void removeCommand(String codigo) {
-        mapCommands.remove(codigo);
-    }
-
-    public Map getMapCommands() {
-        return mapCommands;
     }
 
 }
