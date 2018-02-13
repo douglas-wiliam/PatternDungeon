566063b3b12672820c82fec49cc570f01a8d45bb
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-13 17:13:48 -0300

    Retirada variavel numero de exemplares

--- src/Business/Library/Livro.java
+++ src/Business/Library/Livro.java
@@ -9,18 +9,15 @@ import java.util.ArrayList;
 public class Livro {
 
     protected String codigo;
-    private int numeroExemplares;
     private ArrayList<Exemplar> exemplares;
 
     public Livro(String codigo) {
         this.codigo = codigo;
-        numeroExemplares = 0;
     }
 
     public void addExemplar(Exemplar exemplar) {
         if (!exemplares.contains(exemplar)) {
             exemplares.add(exemplar);
-            numeroExemplares++;
         } else {
             System.out.println("Exemplar já adicionado para este livro");
         }
@@ -29,14 +26,9 @@ public class Livro {
     public void removeExemplar(Exemplar exemplar) {
         if (exemplares.contains(exemplar)) {
             exemplares.remove(exemplar);
-            numeroExemplares--;
         } else {
             System.out.println("Não existe exemplar para este livro");
         }
-    }
-
-    public int getNumeroExemplares() {
-        return numeroExemplares;
     }
 
     public String getCodigo() {
