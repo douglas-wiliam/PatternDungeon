315340be1cf4eff8b599b3fe16175fdae88f9a72
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-15 23:19:16 -0300

    Criadas classes concretas que implementam a interface RealizaOperacao

---
+++ src/Strategy/RealizaEmprestimoFuncionario.java
@@ -0,0 +1,16 @@
+package Strategy;
+
+import Business.Library.Livro;
+
+/**
+ *
+ * @author douglas
+ */
+public class RealizaEmprestimoFuncionario implements RealizaOperacao {
+
+    @Override
+    public String execute(Livro livro) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/Strategy/RealizaReservaAluno.java
@@ -0,0 +1,16 @@
+package Strategy;
+
+import Business.Library.Livro;
+
+/**
+ *
+ * @author douglas
+ */
+public class RealizaReservaAluno implements RealizaOperacao {
+
+    @Override
+    public String execute(Livro livro) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
