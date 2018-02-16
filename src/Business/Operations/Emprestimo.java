e6e937a1f8a52a8dc07ebce281dcae5247c34145
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-16 01:02:11 -0300

    Setado emprestimo e status de Exemplar vinculado a Emprestimo no próprio construtor de Emprestimo. Metodo setStatus alterado para junto a alteracao do Status de Emprestimo alterar o status do Exemplar vinculado

--- src/Business/Operations/Emprestimo.java
+++ src/Business/Operations/Emprestimo.java
@@ -10,47 +10,57 @@ import java.time.format.DateTimeFormatter;
  * @author douglas
  */
 public class Emprestimo {
-
+    
     Usuario usuario;
     Exemplar exemplar;
     String dataCriacao;
     String dataDevolucao;
     String status;
-
+    
     public Emprestimo(Usuario usuario, Exemplar exemplar) {
         this.usuario = usuario;
         this.exemplar = exemplar;
-
+        
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate dataLocal = LocalDate.now();
         
         dataCriacao = dtf.format(dataLocal);
         dataDevolucao = dtf.format(dataLocal.plusDays(usuario.getPeriodoEmprestimo()));
         status = "emCurso";  // "emCurso" ou "finalizado"
-    }
 
+        exemplar.setStatus("emprestado");
+        exemplar.setEmprestimo(this);
+    }
+    
     public Usuario getUsuario() {
         return usuario;
     }
-
+    
     public Exemplar getExemplar() {
         return exemplar;
     }
-
+    
     public String getDataCriacao() {
         return dataCriacao;
     }
-
+    
     public String getDataDevolucao() {
         return dataDevolucao;
     }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
+        if (status.equals("finalizado")) {
+            exemplar.setStatus("disponivel");
+            exemplar.setEmprestimo(null);
+        } else {
+            exemplar.setStatus("emprestado");
+            exemplar.setEmprestimo(this);
+        }
         this.status = status;
     }
-
+    
 }
