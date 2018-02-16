3a7ecaf98575e3eff046a720f5671f5c13995701
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-16 12:58:52 -0300

    Criacao dos metodos privados registraData e atualizaDataDevolucao

--- src/Business/Operations/Emprestimo.java
+++ src/Business/Operations/Emprestimo.java
@@ -10,57 +10,64 @@ import java.time.format.DateTimeFormatter;
  * @author douglas
  */
 public class Emprestimo {
-    
+
     Usuario usuario;
     Exemplar exemplar;
     String dataCriacao;
     String dataDevolucao;
-    String status;
-    
+    String status;  // "emCurso" ou "finalizado"
+
     public Emprestimo(Usuario usuario, Exemplar exemplar) {
         this.usuario = usuario;
         this.exemplar = exemplar;
-        
+    }
+
+    private void registraData() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate dataLocal = LocalDate.now();
-        
+
         dataCriacao = dtf.format(dataLocal);
         dataDevolucao = dtf.format(dataLocal.plusDays(usuario.getPeriodoEmprestimo()));
-        status = "emCurso";  // "emCurso" ou "finalizado"
-
-        exemplar.setStatus("emprestado");
-        exemplar.setEmprestimo(this);
     }
-    
+
+    private void atualizaDataDevolucao() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataLocal = LocalDate.now();
+
+        dataDevolucao = dtf.format(dataLocal);
+    }
+
     public Usuario getUsuario() {
         return usuario;
     }
-    
+
     public Exemplar getExemplar() {
         return exemplar;
     }
-    
+
     public String getDataCriacao() {
         return dataCriacao;
     }
-    
+
     public String getDataDevolucao() {
         return dataDevolucao;
     }
-    
+
     public String getStatus() {
         return status;
     }
-    
+
     public void setStatus(String status) {
         if (status.equals("finalizado")) {
             exemplar.setStatus("disponivel");
             exemplar.setEmprestimo(null);
+            atualizaDataDevolucao();
         } else {
+            registraData();
             exemplar.setStatus("emprestado");
             exemplar.setEmprestimo(this);
         }
         this.status = status;
     }
-    
+
 }
