52da551b2914c2a864f710918010041062ca845c
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-16 11:25:33 -0300

    Criacao das classes RealizaReservaComando e RealizaDevolucaoComando

---
+++ src/Commands/RealizaDevolucaoComando.java
@@ -0,0 +1,23 @@
+package Commands;
+
+import Business.Orders.Pedido;
+import Facade.Fachada;
+
+/**
+ *
+ * @author douglas
+ */
+public class RealizaDevolucaoComando implements Comando {
+
+    private final Pedido pedido;
+
+    public RealizaDevolucaoComando(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public String execute() {
+        return Fachada.realizaDevolucao(pedido);
+    }
+
+}
---
+++ src/Commands/RealizaReservaComando.java
@@ -0,0 +1,23 @@
+package Commands;
+
+import Business.Orders.Pedido;
+import Facade.Fachada;
+
+/**
+ *
+ * @author douglas
+ */
+public class RealizaReservaComando implements Comando {
+
+    private final Pedido pedido;
+
+    public RealizaReservaComando(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public String execute() {
+        return Fachada.realizaReserva(pedido);
+    }
+
+}
