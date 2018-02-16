913b4fd6090dbd033cd0d0b3807270414b3bd2af
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-16 00:05:46 -0300

    Implementação das classes concretas para a Interface TomarEmprestadoBehavior

---
+++ src/Strategy/AlunoTomarEmprestado.java
@@ -0,0 +1,16 @@
+package Strategy;
+
+import Business.Library.Livro;
+
+/**
+ *
+ * @author douglas
+ */
+public class AlunoTomarEmprestado implements TomarEmprestadoBehavior {
+
+    @Override
+    public String tomarEmprestado(Livro livro) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
