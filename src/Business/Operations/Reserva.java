990d705e9ca64990979ae3cfc5497384ccf88517
guilherme <guilherme@localhost>
2018-02-18 18:15:32 -0300

    Implementada a classe Reserva.

--- src/Business/Operations/Reserva.java
+++ src/Business/Operations/Reserva.java
@@ -1,9 +1,40 @@
 package Business.Operations;
 
+import Business.Library.Exemplar;
+import Business.Users.Usuario;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
- * @author douglas
+ * @author douglas, Guilherme
  */
 public class Reserva {
-
+    Usuario usuario;
+    Exemplar exemplar;
+    String dataSolicitacao;
+    
+    public Reserva(Usuario usuario, Exemplar exemplar){
+        this.registraData();
+        this.usuario = usuario;
+        this.exemplar = exemplar;
+    }
+    
+    private void registraData() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataLocal = LocalDate.now();
+        dataSolicitacao = dtf.format(dataLocal);
+    }
+    
+    public Usuario getUsuario(){
+        return this.usuario;
+    }
+    
+    public Exemplar getExemplar(){
+        return this.exemplar;
+    }
+    
+    public String getDataSolicitacao(){
+        return this.dataSolicitacao;
+    }
 }
