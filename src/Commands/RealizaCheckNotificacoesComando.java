00f46af88473a58b210e20fca454f43b3a473e1f
guilherme <guilherme@localhost>
2018-02-18 20:23:04 -0300

    Criada classe RealizaCheckNotificacoesComando

---
+++ src/Commands/RealizaCheckNotificacoesComando.java
@@ -0,0 +1,18 @@
+package Commands;
+
+/**
+ *
+ * @author guilherme
+ */
+public class RealizaCheckNotificacoesComando implements Comando{
+    private final String codigoUsuario;
+    
+    public RealizaCheckNotificacoesComando(String codigoUsuario){
+        this.codigoUsuario = codigoUsuario;
+    }
+    
+    public String execute(){
+        return Fachada.realizaCheckNotificacoes(codigoUsuario);
+    }
+    
+}
