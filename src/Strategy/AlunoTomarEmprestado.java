ca09a51d2befb9cb966d7d68ab45f505e28c1222
Douglas Wiliam <dougl.wil@gmail.com>
2018-02-18 23:56:29 -0300

    Implementado metodo tomarEmprestado

--- src/Strategy/AlunoTomarEmprestado.java
+++ src/Strategy/AlunoTomarEmprestado.java
@@ -1,6 +1,7 @@
 package Strategy;
 
 import Business.Library.Livro;
+import Business.Operations.Emprestimo;
 import Business.Users.Usuario;
 
 /**
@@ -11,8 +12,40 @@ public class AlunoTomarEmprestado implements TomarEmprestadoBehavior {
 
     @Override
     public String tomarEmprestado(Usuario usuario, Livro livro) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return "";
+
+        if (!livro.temExemplarDisponivel()) {   // Exigencia 1
+            return "Insucesso em Operação de Empréstimo: Exemplar não Disponivel na Biblioteca";
+        }
+
+        if (usuario.estaDevedor()) {    // Exigencia 2
+            return "Insucesso em Operação de Empréstimo: Usuário devedor de livro em atraso";
+        }
+
+        if ((usuario.getQtdEmprestimos() + 1) > usuario.getQtdMaxEmprestimos()) { // Exigencia 3
+            return "Insucesso em Operação de Empréstimo: Quantidade máxima de empréstimos excedida";
+        }
+
+        if (livro.getQtdReservas() >= livro.getQtdExemplaresDisponiveis()) { // Exigencia 4
+            if (!usuario.temLivroReservado(livro)) {    // Exigencia 5
+                return "Insucesso em Operação de Empréstimo: Existem reservas na sua frente";
+            }
+        }
+
+        if (usuario.temLivroEmprestado(livro)) {    // Exigencia 6
+            return "Insucesso em Operação de Empréstimo: Usuário possui empréstimo em aberto do livro";
+        }
+
+        if (usuario.temLivroReservado(livro)) {
+            usuario.fechaReserva(livro);
+            livro.fechaReserva(usuario);
+        }
+
+        Emprestimo emprestimo = new Emprestimo(usuario, livro.getExemplarDisponivel());
+        emprestimo.abreEmprestimo();
+
+        livro.addEmprestimo(emprestimo);
+        usuario.addEmprestimo(emprestimo);
+        return "Sucesso em Operação de Empréstimo";
     }
 
 }
